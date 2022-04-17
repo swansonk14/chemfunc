16c5f92a11d67ce691f67b28b94d3355fee59d56
Kyle Swanson <swansonk.14@gmail.com>
2022-04-16 19:09:09 -0700

    Cleaning up clustering script

---
+++ cluster_molecules.py
@@ -0,0 +1,55 @@
+"""Clusters molecules by Morgan fingerprint."""
+from pathlib import Path
+
+import pandas as pd
+from sklearnex import patch_sklearn
+patch_sklearn()
+from sklearn.cluster import KMeans
+from tap import Tap
+
+from morgan_fingerprint import compute_morgan_fingerprints
+
+
+class Args(Tap):
+    data_path: Path  # Path to CSV file containing SMILES.
+    save_path: Path  # Path to CSV file where the clustering will be saved.
+    smiles_column: str = 'smiles'  # Name of the column containing SMILES.
+    num_clusters: int = 50  # Number of clusters.
+
+
+def cluster_molecules(data_path: Path,
+                      save_path: Path,
+                      smiles_column: str = 'smiles',
+                      num_clusters: int = 50) -> None:
+    """Clusters molecules by Morgan fingerprint.
+
+    :param data_path: Path to CSV file containing SMILES.
+    :param save_path: Path to CSV file where the clustering will be saved.
+    :param smiles_column: Name of the column containing SMILES.
+    :param num_clusters: Number of clusters.
+    """
+    print('Loading data')
+    data = pd.read_csv(data_path)
+    print(f'Data size = {len(data):,}')
+
+    print('Computing Morgan fingerprints')
+    morgans = compute_morgan_fingerprints(data[smiles_column])
+
+    print('Clustering')
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(morgans)
+    data['cluster_label'] = kmeans.labels_ + 1
+
+    print('Saving data')
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+    data.to_csv(save_path, index=False)
+
+
+if __name__ == '__main__':
+    args = Args().parse_args()
+
+    cluster_molecules(
+        data_path=args.data_path,
+        save_path=args.save_path,
+        smiles_column=args.smiles_column,
+        num_clusters=args.num_clusters
+    )
