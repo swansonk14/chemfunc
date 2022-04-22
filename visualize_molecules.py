a258061a18e8e0bca20b7963637323605fb7a731
Kyle Swanson <swansonk.14@gmail.com>
2022-04-22 11:18:14 -0700

    Adding option to limit number of molecules that are visualized

--- visualize_molecules.py
+++ visualize_molecules.py
@@ -1,6 +1,7 @@
 """Converts molecule SMILES to grids of images."""
 import math
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 from rdkit import Chem
@@ -15,13 +16,15 @@ class Args(Tap):
     smiles_column: str = 'smiles'  # Name of the column containing SMILES.
     num_rows: int = 4  # Number of rows of molecules/rationales per image.
     mols_per_row: int = 8  # Number of molecules/rationales per row.
+    num_molecules: Optional[int] = None  # Number of molecules to visualize (if None, visualizes all molecules).
 
 
 def visualize_molecules(data_path: Path,
                         save_dir: Path,
                         smiles_column: str = 'smiles',
                         num_rows: int = 4,
-                        mols_per_row: int = 8) -> None:
+                        mols_per_row: int = 8,
+                        num_molecules: Optional[int] = None) -> None:
     """Converts molecule SMILES to grids of images.
 
     :param data_path: Path to CSV file containing SMILES.
@@ -29,6 +32,7 @@ def visualize_molecules(data_path: Path,
     :param smiles_column: Name of the column containing SMILES.
     :param num_rows: Number of rows of molecules/rationales per image.
     :param mols_per_row: Number of molecules/rationales per row.
+    :param num_molecules: Number of molecules to visualize (if None, visualizes all molecules).
     """
     # Load data
     data = pd.read_csv(data_path)
@@ -41,10 +45,12 @@ def visualize_molecules(data_path: Path,
     mols_per_image = num_rows * mols_per_row
     max_index = int(math.ceil(len(data) / mols_per_image))
     num_digits = len(str(max_index))
+    num_molecules = num_molecules or len(data)
+    print(f'Visualizing {num_molecules:,} molecules')
 
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    for i in trange(0, len(data), mols_per_image, desc='Saving images'):
+    for i in trange(0, num_molecules, mols_per_image, desc='Saving images'):
         image_mols = mols[i:i + mols_per_image]
 
         img = Draw.MolsToGridImage(
