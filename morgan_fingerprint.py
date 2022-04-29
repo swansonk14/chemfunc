e517277dffea2967f666d059501a074adf9bbca0
Kyle Swanson <swansonk.14@gmail.com>
2022-04-28 22:17:36 -0700

    Removing extra file

--- morgan_fingerprint.py
+++ morgan_fingerprint.py
@@ -1,4 +1,4 @@
-"""Functions to compute Morgan fingerpritns for molecules."""
+"""Functions to compute Morgan fingerprints for molecules."""
 from functools import partial
 from multiprocessing import Pool
 from typing import Union
