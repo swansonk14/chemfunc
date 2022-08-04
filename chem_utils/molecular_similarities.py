66973c3b8c7dd62d3a902a21a53619e99a4f09cb
Kyle Swanson <swansonk.14@gmail.com>
2022-08-04 13:02:02 -0700

    Compute top_k similarities rather than just max similarity

--- chem_utils/molecular_similarities.py
+++ chem_utils/molecular_similarities.py
@@ -131,10 +131,13 @@ def compute_pairwise_tversky_similarities(mols_1: list[Union[str, Chem.Mol]],
     return tversky_similarities
 
 
-def compute_max_similarities(similarity_type: str,
+def compute_top_similarities(similarity_type: str,
                              mols: list[Union[str, Chem.Mol]],
-                             reference_mols: Optional[list[Union[str, Chem.Mol]]] = None) -> np.ndarray:
-    """Computes the maximum similarity between each molecule and a set of molecules.
+                             reference_mols: Optional[list[Union[str, Chem.Mol]]] = None,
+                             top_k: Union[int, float] = 1) -> np.ndarray:
+    """For each molecule in a list, computes the similarity of the top k most similar molecule in a reference set.
+
+    If top_k == 1, this computes the nearest neighbor similarity.
 
     If only mols is provided, computes the maximum similarity between each molecule and every other molecule in the set.
     If reference_mols is provided, computes the maximum similarity between each molecule in mols and the molecules in
@@ -145,6 +148,11 @@ def compute_max_similarities(similarity_type: str,
     :param similarity_type: The type of similarity to compute between the molecules.
     :param mols: A list of SMILES for the molecules whose similarities should be computed.
     :param reference_mols: A list of SMILES that serves as the reference set against which similarities are computed.
+    :param top_k: This function returns the similarity of the top_k most similar molecule.
+                  If top_k is an int, then the similarity of the top_k most similar molecule is returned
+                  (e.g., top_k = 1 means the similarity of the most similar molecule.)
+                  If top_k is a float, then the similarity of the top_k-th percentile most similar molecule is returned
+                  (e.g., top_k = 0.95 returns the similarity of the 95% percentile most similar molecule).
     :return: The maximum similarity between each molecule and every other molecule (either in mols or reference_mols).
     """
     # Compute pairwise similarities
@@ -154,7 +162,21 @@ def compute_max_similarities(similarity_type: str,
     if reference_mols is None:
         np.fill_diagonal(pairwise_similarities, -np.inf)
 
-    # Compute maximum similarity between each molecule and the other molecules
-    max_pairwise_similarities = np.max(pairwise_similarities, axis=1)
+    # Compute similarity between each molecule and the other molecules
+    if isinstance(top_k, int):
+        if top_k == 1:
+            similarities = np.max(pairwise_similarities, axis=1)
+        elif 1 <= top_k <= len(reference_mols):
+            argsort = np.argsort(pairwise_similarities)
+            top_k_mask = argsort == (len(reference_mols) - top_k)  # argsort starts as 0 and ascends, opposite of top_k
+            similarities = pairwise_similarities[top_k_mask]
+        else:
+            raise ValueError(f'top_k must be between 1 and {len(reference_mols)} '
+                             f'(length of reference_mols) but got {top_k}')
 
-    return max_pairwise_similarities
+    elif isinstance(top_k, float):
+        similarities = np.percentile(pairwise_similarities, top_k, axis=1)
+    else:
+        raise ValueError(f'top_k must be either an int or a float but got {type(top_k)}')
+
+    return similarities
