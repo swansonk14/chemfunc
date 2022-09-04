da65512c670cde6ca9a18346296b335071cfd159
Kyle Swanson <swansonk.14@gmail.com>
2022-09-04 14:15:44 -0700

    Fixed sorting order when selecting from clusters

--- chem_utils/select_from_clusters.py
+++ chem_utils/select_from_clusters.py
@@ -6,25 +6,25 @@ import pandas as pd
 from chem_utils.constants import CLUSTER_COLUMN
 
 
-def filter_molecules(data_path: Path,
-                     save_path: Path,
-                     value_column: str,
-                     cluster_column: str = CLUSTER_COLUMN,
-                     descending: bool = False) -> None:
+def select_from_clusters(data_path: Path,
+                         save_path: Path,
+                         value_column: str,
+                         cluster_column: str = CLUSTER_COLUMN,
+                         ascending: bool = False) -> None:
     """Selects the best molecule from each cluster.
 
     :param data_path: Path to CSV file containing molecules.
     :param save_path: Path to CSV file where selected molecules will be saved.
     :param value_column: Name of the column containing values to sort molecules by.
     :param cluster_column: Name of the column containing cluster labels.
-    :param descending: Sorts molecules in each cluster from highest to lowest rather than lowest to highest.
+    :param ascending: Sorts molecules in each cluster from lowest to highest rather than highest to lowest.
     """
     # Load data
     data = pd.read_csv(data_path)
     print(f'Original data size = {len(data):,}')
 
     # Select molecules
-    data.sort_values(by=value_column, ascending=not descending, inplace=True)
+    data.sort_values(by=value_column, ascending=ascending, inplace=True)
     data.drop_duplicates(subset=[cluster_column], inplace=True)
     print(f'Final data size = {len(data):,}')
 
@@ -41,6 +41,6 @@ if __name__ == '__main__':
         save_path: Path  # Path to CSV file where selected molecules will be saved.
         value_column: str  # Name of the column containing values to sort molecules by.
         cluster_column: str = CLUSTER_COLUMN  # Name of the column containing cluster labels.
-        descending: bool = False  # Sorts molecules in each cluster from highest to lowest rather than lowest to highest.
+        ascending: bool = False  # Sorts molecules in each cluster from lowest to highest rather than highest to lowest.
 
-    filter_molecules(**Args().parse_args().as_dict())
+    select_from_clusters(**Args().parse_args().as_dict())
