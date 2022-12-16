886b5aef246a33b78a4531eff502c1541e18cb7e
Kyle Swanson <swansonk.14@gmail.com>
2022-12-16 10:12:38 -0800

    Fixing chemical diversity script and enabling tanimoto and tversky distances

--- chem_utils/chemical_diversity.py
+++ chem_utils/chemical_diversity.py
@@ -1,46 +1,49 @@
-"""Computes the chemical diversity of a list of molecules in terms of Tanimoto distances."""
+"""Computes the chemical diversity of a list of molecules in terms of chemical distances."""
 from pathlib import Path
+from typing import Literal
 
 import numpy as np
 import pandas as pd
 
 from chem_utils.constants import SMILES_COLUMN
-from chem_utils.nearest_neighbor import compute_pairwise_tanimoto_similarities
+from chem_utils.molecular_similarities import get_similarity_function
 
 
 def chemical_diversity(data_path: Path,
-                       smiles_column: str = SMILES_COLUMN) -> None:
-    """Computes the chemical diversity of a list of molecules in terms of Tanimoto distances.
+                       smiles_column: str = SMILES_COLUMN,
+                       similarity_type: Literal['tanimoto', 'tversky'] = 'tanimoto') -> None:
+    """Computes the chemical diversity of a list of molecules in terms of chemical distances.
 
     Note: Does NOT remove duplicate SMILES before computing pairwise distances.
 
     :param data_path: Path to CSV file containing molecule SMILES.
     :param smiles_column: Name of the column containing SMILES.
+    :param similarity_type: Type of similarity to use when computing distances.
     """
     # Load data
     data = pd.read_csv(data_path)
     print(f'Number of molecules = {len(data):,}')
 
-    # Compute pairwise Tanimoto distances
-    pairwise_tanimoto_distances = compute_pairwise_tanimoto_similarities(mols_1=data[smiles_column])
+    # Compute pairwise distances
+    pairwise_distances = 1 - get_similarity_function(similarity_type)(mols_1=data[smiles_column])
 
-    # Compute average Tanimoto distance between pairs
-    unique_distances_mask = np.triu(np.ones(pairwise_tanimoto_distances.shape), k=1).astype(bool)
-    unique_pairwise_tanimoto_distances = pairwise_tanimoto_distances[unique_distances_mask]
-    average_tanimoto_distance = np.mean(unique_pairwise_tanimoto_distances)
-    std_tanimoto_distance = np.std(unique_pairwise_tanimoto_distances)
+    # Compute average distance between pairs
+    unique_distances_mask = np.triu(np.ones(pairwise_distances.shape), k=1).astype(bool)
+    unique_pairwise_distances = pairwise_distances[unique_distances_mask]
+    average_distance = np.mean(unique_pairwise_distances)
+    std_distance = np.std(unique_pairwise_distances)
 
-    print(f'Average Tanimoto distance between pairs = '
-          f'{average_tanimoto_distance:.3f} +/- {std_tanimoto_distance:.3f}')
+    print(f'Average {similarity_type.title()} distance between pairs = '
+          f'{average_distance:.3f} +/- {std_distance:.3f}')
 
-    # Compute average minimum Tanimoto distance between each molecule and the other molecules
-    np.fill_diagonal(pairwise_tanimoto_distances, np.inf)
-    min_tanimoto_distances = np.min(pairwise_tanimoto_distances, axis=1)
-    average_min_tanimoto_distance = np.mean(min_tanimoto_distances)
-    std_min_tanimoto_distance = np.std(min_tanimoto_distances)
+    # Compute average minimum distance between each molecule and the other molecules
+    np.fill_diagonal(pairwise_distances, np.inf)
+    min_distances = np.min(pairwise_distances, axis=1)
+    average_min_distance = np.mean(min_distances)
+    std_min_distance = np.std(min_distances)
 
-    print(f'Average minimum Tanimoto distance from each molecule to the rest = '
-          f'{average_min_tanimoto_distance:.3f} +/- {std_min_tanimoto_distance:.3f}')
+    print(f'Average minimum {similarity_type.title()} distance from each molecule to the rest = '
+          f'{average_min_distance:.3f} +/- {std_min_distance:.3f}')
 
 
 if __name__ == '__main__':
@@ -49,5 +52,6 @@ if __name__ == '__main__':
     class Args(Tap):
         data_path: Path  # Path to CSV file containing molecule SMILES.
         smiles_column: str = SMILES_COLUMN  # Name of the column containing SMILES.
+        similarity_type: Literal['tanimoto', 'tversky'] = 'tanimoto'  # Type of similarity to use when computing distances.
 
     chemical_diversity(**Args().parse_args().as_dict())
