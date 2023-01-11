694275b3cd36543a6fd77879c93e524454931e85
Kyle Swanson <swansonk.14@gmail.com>
2023-01-11 17:31:13 -0500

    Saving property distribution plot data

--- chem_utils/property_distribution.py
+++ chem_utils/property_distribution.py
@@ -1,9 +1,10 @@
-"""Computes the distribution of molecular weights over a set of molecules."""
+"""Computes the distribution of property values over a set of molecules."""
 from multiprocessing import Pool
 from pathlib import Path
 from typing import Literal
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem.Crippen import MolLogP
@@ -38,54 +39,65 @@ def molecular_weight(smiles: str) -> float:
 
 
 def property_distribution(data_paths: list[Path],
-                          save_path: Path,
-                          property: Literal['mol_weight', 'logp'],
+                          save_dir: Path,
+                          prop: Literal['mol_weight', 'logp'],
                           smiles_column: str = SMILES_COLUMN,
                           min_value: float = -float('inf'),
-                          max_value: float = float('inf')) -> None:
-    """Computes the distribution of molecular weights over a set of molecules.
+                          max_value: float = float('inf'),
+                          save_data: bool = False) -> None:
+    """Computes the distribution of property values over a set of molecules.
 
     :param data_paths: Path to CSV files containing SMILES.
+    :param save_dir: Path to a directory where the plot and data will be saved.
+    :param prop: The name of the property to compute.
     :param smiles_column: The name of the column in data_path containing SMILES.
-    :param property: The name of the property to compute.
-    :param save_path: Path to a PDF file where the plot of molecular weights will be saved.
-    :param min_value: Minimum molecular weight to plot (removes outliers).
-    :param max_value: Maximum molecular weight to plot (removes outliers).
+    :param min_value: Minimum property value to plot (removes outliers).
+    :param max_value: Maximum property value to plot (removes outliers).
+    :param save_data: Whether to save the property data as a CSV alongside the plot.
     """
     # Select property function
-    if property == 'mol_weight':
-        property_fn = molecular_weight
-    elif property == 'logp':
-        property_fn = logP
+    if prop == 'mol_weight':
+        prop_fn = molecular_weight
+    elif prop == 'logp':
+        prop_fn = logP
     else:
-        raise ValueError(f'Molecular property "{property}" is not supported.')
+        raise ValueError(f'Molecular property "{prop}" is not supported.')
 
     # Iterate over data paths
+    prop_data = {}
     for data_path in tqdm(data_paths, desc='Data paths'):
         # Load data
         data = pd.read_csv(data_path)
 
-        # Compute molecular weights
+        # Compute property values
         with Pool() as pool:
-            values = [
-                value
-                for value in tqdm(pool.imap_unordered(property_fn, data[smiles_column]),
-                                       total=len(data), desc=property)
-                if min_value <= value <= max_value
-            ]
+            data[prop] = list(tqdm(pool.imap(prop_fn, data[smiles_column]), total=len(data), desc=prop))
 
-        # Plot molecular weights
-        plt.hist(values, bins=100, density=True, label=data_path.stem, alpha=1 / len(data_paths))
+        # Plot property values
+        plt.hist(data[prop][(data[prop] >= min_value) & (data[prop] <= max_value)],
+                 bins=100, density=True, label=data_path.stem, alpha=1 / len(data_paths))
+
+        # Save data with property values
+        prop_data[data_path.stem] = data[prop]
 
     # Label plot
-    plt.xlabel(property)
+    plt.xlabel(prop)
     plt.ylabel('Density')
-    plt.title(f'{property} Distribution')
+    plt.title(f'{prop} Distribution')
     plt.legend()
 
     # Save plot
-    save_path.parent.mkdir(parents=True, exist_ok=True)
-    plt.savefig(save_path, bbox_inches='tight')
+    save_dir.mkdir(parents=True, exist_ok=True)
+    plt.savefig(save_dir / f'{prop}.pdf', bbox_inches='tight')
+
+    # Save data
+    if save_data:
+        max_len = max(len(values) for values in prop_data.values())
+        coordinates = pd.DataFrame({
+            key: np.pad(values, (0, max_len - len(values)), constant_values=np.nan)
+            for key, values in prop_data.items()
+        })
+        coordinates.to_csv(save_dir / f'{prop}.csv', index=False)
 
 
 if __name__ == '__main__':
@@ -93,10 +105,11 @@ if __name__ == '__main__':
 
     class Args(Tap):
         data_paths: list[Path]  # Path to CSV files containing SMILES.
-        save_path: Path  # Path to a PDF file where the plot of molecular weights will be saved.
-        property: Literal['mol_weight', 'logp']  # The name of the property to compute.
+        save_dir: Path  # Path to a directory where the plot and (optionally) data will be saved.
+        prop: Literal['mol_weight', 'logp']  # The name of the property to compute.
         smiles_column: str = SMILES_COLUMN  # The name of the column in data_paths containing SMILES.
         min_value: float = -float('inf')  # Minimum property value to plot (removes outliers).
         max_value: float = float('inf')  # Maximum property value to plot (removes outliers).
+        save_data: bool = False  # Whether to save the property data as a CSV alongside the plot.
 
     property_distribution(**Args().parse_args().as_dict())
