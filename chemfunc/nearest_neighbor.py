ef57b6c3942447669c37e8b94a8f16f7a6207de3
Kyle Swanson <swansonk.14@gmail.com>
2023-06-02 08:51:04 -0700

    Nearest neighbor metric fix

--- chemfunc/nearest_neighbor.py
+++ chemfunc/nearest_neighbor.py
@@ -12,7 +12,7 @@ from chemfunc.molecular_similarities import get_similarity_function
 def nearest_neighbor(
         data_path: Path,
         reference_data_path: Path,
-        metrics: tuple[Literal['tanimoto', 'mcs', 'tversky'], ...] = ('tanimoto',),
+        metric: Literal['tanimoto', 'mcs', 'tversky'] = 'tanimoto',
         save_path: Path | None = None,
         smiles_column: str = SMILES_COLUMN,
         reference_smiles_column: str | None = None,
@@ -22,11 +22,11 @@ def nearest_neighbor(
 
     :param data_path: Path to CSV file containing data with SMILES whose neighbors are to be computed.
     :param reference_data_path: Path to CSV file containing reference SMILES which will be the neighbors of data_path.
-    :param metrics: Metrics to use when computing similarity.
-                    tanimoto: Tanimoto similarity using Morgan fingerprint.
-                    mcs: Maximum common substructure as a proportion of the number of atoms in the reference molecule.
-                    tversky: Tversky index with alpha = 0 and beta = 1, i.e., the proportion of reference substructures
-                    in the molecule.
+    :param metric: Metric to use when computing similarity.
+                   tanimoto: Tanimoto similarity using Morgan fingerprint.
+                   mcs: Maximum common substructure as a proportion of the number of atoms in the reference molecule.
+                   tversky: Tversky index with alpha = 0 and beta = 1, i.e., the proportion of reference substructures
+                   in the molecule.
     :param save_path: Where the data with the neighbor info should be saved (defaults to data_path).
     :param smiles_column: Name of the column in data_path containing SMILES.
     :param reference_smiles_column: Name of the column in reference_data_path containing SMILES.
@@ -49,26 +49,25 @@ def nearest_neighbor(
     reference_data.drop_duplicates(subset=reference_smiles_column, inplace=True)
     reference_data.sort_values(by=reference_smiles_column, ignore_index=True, inplace=True)
 
-    for metric in metrics:
-        print(f'Computing similarities using {metric} metric')
-        similarities = get_similarity_function(metric)(
-            data[smiles_column],
-            reference_data[reference_smiles_column]
-        )
+    print(f'Computing similarities using {metric} metric')
+    similarities = get_similarity_function(metric)(
+        data[smiles_column],
+        reference_data[reference_smiles_column]
+    )
 
-        print('Finding minimum distance SMILES')
-        prefix = f'{f"{reference_name}_" if reference_name is not None else ""}{metric}_'
+    print('Finding minimum distance SMILES')
+    prefix = f'{f"{reference_name}_" if reference_name is not None else ""}{metric}_'
 
-        max_similarity_indices = np.argmax(similarities, axis=1)
+    max_similarity_indices = np.argmax(similarities, axis=1)
 
-        data[f'{prefix}nearest_neighbor'] = [
-            reference_data[reference_smiles_column][max_similarity_index]
-            for max_similarity_index in max_similarity_indices
-        ]
-        data[f'{prefix}nearest_neighbor_similarity'] = [
-            similarities[i, max_similarity_index]
-            for i, max_similarity_index in enumerate(max_similarity_indices)
-        ]
+    data[f'{prefix}nearest_neighbor'] = [
+        reference_data[reference_smiles_column][max_similarity_index]
+        for max_similarity_index in max_similarity_indices
+    ]
+    data[f'{prefix}nearest_neighbor_similarity'] = [
+        similarities[i, max_similarity_index]
+        for i, max_similarity_index in enumerate(max_similarity_indices)
+    ]
 
     print('Saving')
     save_path.parent.mkdir(parents=True, exist_ok=True)
