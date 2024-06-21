3aee73fba16158a664c8e2fb7ce2e515a1a36c58
Kyle Swanson <swansonk.14@gmail.com>
2024-06-21 18:42:19 +0200

    Bump version to 1.0.8

--- chemfunc/_version.py
+++ chemfunc/_version.py
@@ -1,6 +1,6 @@
 """Contains the version information for chemfunc."""
 # major, minor, patch
-version_info = 1, 0, 7
+version_info = 1, 0, 8
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
