74706e42719396f2559f60b69b4db7f5a57a6580
Kyle Swanson <swansonk.14@gmail.com>
2024-07-08 19:37:23 +0200

    Adding PAINS and other unwanted substructures filter and bumping version to 1.0.9

--- chemfunc/_version.py
+++ chemfunc/_version.py
@@ -1,6 +1,6 @@
 """Contains the version information for chemfunc."""
 # major, minor, patch
-version_info = 1, 0, 8
+version_info = 1, 0, 9
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
--- chemfunc/molecular_properties.py
+++ chemfunc/molecular_properties.py
@@ -5,10 +5,10 @@ from typing import Callable
 from rdkit import Chem
 from rdkit.Chem.Crippen import MolLogP
 from rdkit.Chem.Descriptors import MolWt
+from rdkit.Chem.FilterCatalog import FilterCatalog, FilterCatalogParams
 from rdkit.Chem.QED import qed
 
 from chemfunc.constants import Molecule
-
 
 PropertyFunction = Callable[[Molecule], float]
 PROPERTY_FUNCTION_REGISTRY = {}
@@ -20,6 +20,7 @@ def register_property_function(property_type: str) -> Callable[[PropertyFunction
     :param property_type: The name to use to access the property function.
     :return: A decorator which will add a property function to the registry using the specified name.
     """
+
     def decorator(property_function: PropertyFunction) -> PropertyFunction:
         PROPERTY_FUNCTION_REGISTRY[property_type] = property_function
         return property_function
@@ -46,6 +47,7 @@ def get_available_property_functions() -> list[str]:
 
 def smiles_to_mol_wrapper(property_function: PropertyFunction) -> PropertyFunction:
     """A decorator which converts a SMILES to an RDKit molecule before passing it to a property function."""
+
     @wraps(property_function)
     def wrapper(molecule: Molecule) -> float:
         if isinstance(molecule, str):
@@ -89,6 +91,27 @@ def compute_qed(molecule: Molecule) -> float:
     return qed(molecule)
 
 
+@register_property_function('pains_plus')
+@smiles_to_mol_wrapper
+def compute_pains_plus(molecule: Molecule) -> str:
+    """Checks a molecule for PAINS (pan-assay interference compounds) and other unwanted substructures.
+
+    :param molecule: A molecule, either a SMILES string or an RDKit molecule.
+    :return: The PAINS and other unwanted substructure(s) found in the molecule.
+    """
+    # Set up all unwanted substructures filters
+    params = FilterCatalogParams()
+    params.AddCatalog(FilterCatalogParams.FilterCatalogs.ALL)
+    catalog = FilterCatalog(params)
+
+    # Check for unwanted substructures
+    matches = " | ".join(
+        f"{entry.GetProp('FilterSet')}: {entry.GetDescription()}" for entry in catalog.GetMatches(molecule)
+    )
+
+    return matches
+
+
 try:
     import os
     import sys
@@ -98,6 +121,7 @@ try:
     sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
 
     import sascorer
+
 
     @register_property_function('sa_score')
     @smiles_to_mol_wrapper
